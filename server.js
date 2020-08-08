0db81b733170537e187a437e34ecb52422a828d0
Dorayaki99 <rdorayaki99@gmail.com>
2020-08-08 14:16:17 +0700

    server mern

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const connectDB = require('./config/database');
+const { check, validationResult } = require('express-validator/check');
 
 const app = express();
 
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/users', require('./routers/api/users'));
+app.use('/api/auth', require('./routers/api/auth'));
+app.use('/api/profile', require('./routers/api/profile'));
+app.use('/api/posts', require('./routers/api/posts'));
 
 const PORT = process.env.PORT || 1616;
 
